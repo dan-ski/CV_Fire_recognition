d46d96e91f689d0286b43f99d1ef489a8214de2a
Szt.Danski <szt.danski@gmail.com>
2022-11-09 22:27:51 +0100

    Processing data.

--- data.py
+++ data.py
@@ -1,12 +1,16 @@
 import cv2
+import numpy as np
+
 import glob
 import pandas as pd
 import numpy as np
 from tensorflow import keras
 
-PATH_FIRE = r'C:\Users\szturminskid\Documents\Order Soft\Daniel\Fire\archive\fire_dataset\fire_images'
+#PATH_FIRE = r'C:\Users\szturminskid\Documents\Order Soft\Daniel\Fire\archive\fire_dataset\fire_images'
+PATH_FIRE = r'D:\Data Science\Fire recognition- computer vision\archive\fire_dataset\fire_images'
 
-PATH_NOFIRE = r'C:\Users\szturminskid\Documents\Order Soft\Daniel\Fire\archive\fire_dataset\non_fire_images'
+#PATH_NOFIRE = r'C:\Users\szturminskid\Documents\Order Soft\Daniel\Fire\archive\fire_dataset\non_fire_images'
+PATH_NOFIRE = r'D:\Data Science\Fire recognition- computer vision\archive\fire_dataset\non_fire_images'
 
 fire_image_example = r'C:\Users\szturminskid\Documents\Order Soft\Daniel\Fire\archive\fire_dataset\fire_images\fire.1.png'
 
@@ -14,15 +18,29 @@ fire_image_example = r'C:\Users\szturminskid\Documents\Order Soft\Daniel\Fire\ar
 
 # images = [cv2.imread(file) for file in glob.glob("path/to/files/*.png")]
 
-def load_data(path):
-    fire_img = cv2.imread(fire_image_example)
 
-    return fire_img
+def load_data(images_path):
+    #fire_img = cv2.imread(fire_image_example)
+    images = [cv2.imread(file) for file in glob.glob(f"{images_path}/*.png")]
+    return images
 
-fire_image = load_data(fire_image_example)
+def show_image(image):
 
-window_name = 'image'
+    window_name = 'image'
+    cv2.imshow(window_name, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-cv2.imshow(window_name, fire_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def print_type(data):
+    print('Type: ',type(data))
+
+def train_test_sets(x_images, y_images):
+
+fire_images = load_data(PATH_FIRE)
+nonfire_images = load_data(PATH_NOFIRE)
+
+show_image(fire_images[0])
+print_type(fire_images)
+print(len(fire_images))
+
+
